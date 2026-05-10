2c13b065d26818e13331edddd5d8f01840ecc454
Rahaf Taha <rahaftaha594@example.com>
2026-05-10 15:20:27 +0200

    MVP

---
+++ backend/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+//api.php
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/users', [UserController::class, 'index']);
+});
