eb5d243b1890053ab0980f6e279b6bbdb1f3ece5
mahmoudbaddour910 <mahmoudbadour698@gmail.com>
2026-06-02 11:32:05 +0300

    add get latest 6 transactions API endpoint

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,7 +2,8 @@
 // app/Http/Controllers/UserController.php
 
 namespace App\Http\Controllers;
-
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class UserController extends Controller
@@ -12,4 +13,20 @@ class UserController extends Controller
         $users = User::all(['id', 'name', 'email']);
         return response()->json($users);
     }
+    public function getLatestTransactions()
+{
+    $user = Auth::user();
+
+    $transactions = Transaction::where('customer_id', $user->id)
+        ->with(['transfer.sender', 'transfer.receiver']) 
+        ->latest()
+        ->take(6)
+        ->get();
+
+    return response()->json([
+        'success' => true,
+        'message' => 'تم جلب آخر 6 عمليات بنجاح مع تفاصيلها.',
+        'data' => $transactions
+    ], 200);
+}
 }
